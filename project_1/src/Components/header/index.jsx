57f095ad4de1d8ee3386dec10d1a9c2bc932b40b
Rev <rkeveyan23@gmail.com>
2023-01-05 21:58:37 +0400

    comment

---
+++ project_1/src/Components/header/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component{
+
+
+
+    render (){
+        return <div>
+            <p>Hello World</p>
+        </div>
+    }
+}
+
+export default Header
